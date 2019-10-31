a49d4f190f1891422b651e95114cf29e7db8e947
Your Name <you@example.com>
2019-10-31 09:26:03 +0800

    update

--- snake/snake.cpp
+++ snake/snake.cpp
@@ -28,9 +28,9 @@ Position g_food;
 void Initfood()
 {
 	srand((unsigned)time(NULL));
-		g_food.x = rand() % MAP_WIDTH;
-		g_food.y = rand() % MAP_HEIGTH;
-		DrawChar(g_food.x, g_food.y, '#');
+	g_food.x = rand() % MAP_WIDTH;
+	g_food.y = rand() % MAP_HEIGTH;
+	DrawChar(g_food.x, g_food.y, '#');
 }
 
 void InitSnake()
@@ -88,30 +88,70 @@ void InitMap()
 	}
 }
 
-	
+void Init()
+{
+	InitSnake();
+	InitMap();
+	Initfood();
+}
 
-	void Init()
+void SnakeMove(int key)
+{
+	int delta_x = 0;
+	int delta_y = 0;
+	if (key == 'w' || key == 'W')
 	{
-		InitSnake();
-		InitMap();
-		Initfood();
+		delta_x = 0;
+		 delta_y = -1;
 	}
+	else if (key == 's' || key == 'S')
+	{
+		delta_x = 0;
+		delta_y = 1;
+	}
+	else if (key == 'a' || key == 'A')
+	{
+		delta_x = -1;
+		delta_y = 0;
+	}
+	else if (key == 'd' || key == 'D')
+	{
+		delta_x = 1;
+		delta_y = 0;
+	}
+	else
+	{
+		return;
+	}
+	DrawChar(g_snake.pos[g_snake.size - 1].x, g_snake.pos[g_snake.size - 1].y, ' ');
+	for (int i = g_snake.size - 1; i > 0; i--)
+	{
+		g_snake.pos[i].x = g_snake.pos[i - 1].x;
+		g_snake.pos[i].y = g_snake.pos[i - 1].y;
+	}
+	g_snake.pos[0].x += delta_x;
+	g_snake.pos[0].y += delta_y;
+
+}
 
 void GameLoop()
 {
+	int key = 0;
 	while (1)
 	{
 		if (_kbhit())
 		{
-			int key = getchar();
+			 key = _getch();
+		}
 			if (key == 'q' || key == 'Q')
 			{
+				return;
+			}
 
-			}return ;
-		}
+		SnakeMove(key);
 		DrawSnake();
 		Sleep(100);
-}
+	}
 }
 
 void Score()
@@ -124,7 +164,7 @@ int main(int argc, char* argv[])
 {
 	Init();
 	GameLoop();
-	
+
 	Score();
-    return 0;
+	return 0;
 }
